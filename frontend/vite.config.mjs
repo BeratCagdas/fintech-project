5b6b03e1e4117e7d3604f8b675d14631fa023ecf
BeratCagdas <135677473+BeratCagdas@users.noreply.github.com>
2025-11-10 02:13:27 +0300

    upgrade
    fix: add allowedHosts for render

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,6 +1,12 @@
-// vite.config.mjs (Render safe version)
-export default {
-  plugins: [
-    (await import("@vitejs/plugin-react")).default()
-  ],
-};
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    allowedHosts: ["fintech-frontend-8nux.onrender.com"],
+    port: process.env.PORT || 10000,
+    host: true,
+  },
+});
